626421072cfc1454228c3b15f23bd363fbcd1e4d
Yip Ka Yan <yipkayan3@gmail.com>
2022-07-18 17:52:06 +0800

    initial commit from spring initializr

---
+++ src/main/java/dev/codes/ttdbyexample/TtdByExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.codes.ttdbyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtdByExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtdByExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/codes/ttdbyexample/TtdByExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codes.ttdbyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtdByExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
